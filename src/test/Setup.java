0568754cd058044de410ba49cfa404f4c5e08f3a
Jazz <theautomationtechies@gmail.com>
2021-11-11 16:22:02 -0500

    Added new Testng.xml and mvn goal

---
+++ src/test/Setup.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Setup {
+
+    /*
+    1. Download the Maven project
+    2. Download ChromeDriver and Firefox Driver
+    3. Write the Test case
+    4. Go to www.google.ca
+    5. Print the page title, page source and current URL
+    6. Try the Navigate commands
+    7. Brush up HTML tags
+     */
+}
